b957f0fd14d11b5efefb28a5625a7224f91a8471
송재우 <wastes-curates-7y@icloud.com>
2026-05-15 21:28:41 +0900

    test_protocol/util/logger

--- common/protocol.py
+++ common/protocol.py
@@ -25,15 +25,15 @@ info_rsp = {
 
 dns_rqst = {
     "type": "dns_rqst",
-    "name": str,
+    "url_name": str,
     "txid": int, # 보안 모듈에서 사용하기 위해 미리 추가
 }
 
 dns_rsp = {
     "type": "dns_rsp",
-    "name": str,
+    "url_name_ehco": str, # 질의 echo(식별 위해 클라이언트 질의 그대로 돌려줌)
     "txid": int,
-    "answer": str | dict,
+    "url_answer": str | dict,
 }
 
 chunk_rqst = {
---
+++ test_common.py
@@ -0,0 +1,31 @@
+"""
+protocol, time_utils, logger 테스트
+
+"""
+
+from common.protocol import pack, unpack
+from common.time_utils import now_time, time_to_ms, elapsed_time
+from common.logger import get_logger
+
+def test_protocol():
+    test_msg = [
+        {"type": "info_rqst", "movie_id": 1},
+        {"type": "info_rsp", "movie_id": 1, "index_url": "https://index.netplus.com/1234"},
+        
+        {"type": "dns_rqst", "url_name": "index.netplus.com/1234", "txid": 42},
+        {"type": "dns_rsp", "url_name_echo": "index.netplus.com/1234", "txid": 42, "answer": "abCDN.net/"},
+        # manifest
+        {"type": "dns_rsp", "url_name_echo": "abCDN.net/", "txid": 99,
+         "answer": {"HQ": "127.0.0.1:50000", "MQ": "127.0.0.1:50001", "LQ": "127.0.0.1:50002"}},
+        
+        {"type": "chunk_rqst", "movie_id": 1, "encoding_type": "HQ", "chunk_index": 0,
+         "last_watched_time": "00:00:00:000"},
+        {"type": "chunk_rsp", "movie_id": 1, "encoding_type": "HQ", "chunk_index": 0,
+         "start_time": "00:00:00:000", "end_time": "00:00:04:000", "payload_size": 1024},
+    ]
+
+    for i in test_msg:
+        assert unpack(pack(i)) == i, f"FAIL: {i}"
+    print("protocol roundtrip OK")
+
+test_protocol()
